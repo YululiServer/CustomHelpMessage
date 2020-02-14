0aaaeab0f7c06f8629026accd196d80b9fe5e09d
acrylic-style <me@acrylicstyle.xyz>
2020-02-14 09:36:52 +0900

    no longer requires lib

--- src/main/java/xyz/acrylicstyle/chm/CustomHelpMessage.java
+++ src/main/java/xyz/acrylicstyle/chm/CustomHelpMessage.java
@@ -6,22 +6,18 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import xyz.acrylicstyle.tomeito_core.providers.ConfigProvider;
-import xyz.acrylicstyle.tomeito_core.utils.Log;
 
 public class CustomHelpMessage extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        Log.info("Enabled CustomHelpMessage");
     }
 
     @EventHandler
     public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent e) {
         if (e.getMessage().startsWith("/help")) {
             e.setCancelled(true);
-            ConfigProvider config = ConfigProvider.initWithoutException("./plugins/CustomHelpMessage/config.yml");
-            config.getStringList("messages").forEach(s -> e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', s)));
+            this.getConfig().getStringList("messages").forEach(s -> e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', s)));
         }
     }
 }
