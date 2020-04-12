384184c0ba1f7a6d4007a1a65909800ab73f6a2a
acrylic-style <me@acrylicstyle.xyz>
2020-04-12 19:42:16 +0900

    join message

--- src/main/java/xyz/acrylicstyle/chm/CustomHelpMessage.java
+++ src/main/java/xyz/acrylicstyle/chm/CustomHelpMessage.java
@@ -5,7 +5,10 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
 
 public class CustomHelpMessage extends JavaPlugin implements Listener {
     @Override
@@ -14,10 +17,19 @@ public class CustomHelpMessage extends JavaPlugin implements Listener {
     }
 
     @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        List<String> list = this.getConfig().getStringList("joinMessage");
+        if (list == null || list.size() == 0) return;
+        list.forEach(s -> e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', s)));
+    }
+
+    @EventHandler
     public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent e) {
         if (e.getMessage().startsWith("/help")) {
             e.setCancelled(true);
-            this.getConfig().getStringList("messages").forEach(s -> e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', s)));
+            List<String> list = this.getConfig().getStringList("messages");
+            if (list == null || list.size() == 0) return;
+            list.forEach(s -> e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', s)));
         }
     }
 }
